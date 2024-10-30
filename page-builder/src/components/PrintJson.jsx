380944f2eb1db25bdf31eb3fa53318c2e8ec8b1a
shravanjoshihydi <115337299+shravanjoshihydi@users.noreply.github.com>
2024-10-30 19:04:25 +0530

    Update PrintJson.jsx

--- page-builder/src/components/PrintJson.jsx
+++ page-builder/src/components/PrintJson.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import { useEditor } from "@craftjs/core";
 import lz from "lzutf8";
 
@@ -20,7 +21,8 @@ export const PrintJson = () => {
         };
       }
       if (props?.componentName != undefined) {
-        component.componentName = component.widgetprops.componentName.toLowerCase();
+        component.componentName =
+          component.widgetprops.componentName.toLowerCase();
         component.version = component.widgetprops.version;
         delete component.widgetprops.componentName;
         delete component.widgetprops.version;
@@ -29,7 +31,7 @@ export const PrintJson = () => {
         let objKeys = Object.keys(node.linkedNodes);
         objKeys.forEach((key) => {
           const layoutNode = allNodes[node.linkedNodes[key]];
-          if (layoutNode.nodes && layoutNode.nodes.length > 0) {
+          if (layoutNode?.nodes && layoutNode?.nodes.length > 0) {
             const layoutNodeprops = layoutNode.props;
             layoutNode.nodes.forEach((childNodeId) => {
               const childNode = allNodes[childNodeId];
@@ -60,21 +62,59 @@ export const PrintJson = () => {
     }
     const allNodes = json;
     const rootComponent = json["ROOT"];
-    const result = processComponent(rootComponent, allNodes);
-    return result.children;
+
+    function returnNodeList(nodesList, objectName) {
+      nodesList.forEach((key) => {
+        objectName[key] = allNodes[key];
+
+        function giveParentNode(nodeId) {
+          const node = allNodes[nodeId];
+          if (!node) return;
+          objectName[nodeId] = node;
+          giveParentNode(node.parent);
+        }
+
+        giveParentNode(allNodes[key].parent);
+      });
+    }
+
+    const SSRNodesList = {};
+
+    const SSRNodes = Object.keys(allNodes).filter(
+      (key) =>
+        allNodes[key]?.props?.isSSR && allNodes[key]?.props?.isSSR === "TRUE"
+    );
+    returnNodeList(SSRNodes, SSRNodesList);
+
+    const CSRNodesList = {};
+
+    const CSRNodes = Object.keys(allNodes).filter(
+      (key) =>
+        allNodes[key]?.props?.isSSR && allNodes[key]?.props?.isSSR !== "TRUE"
+    );
+    returnNodeList(CSRNodes, CSRNodesList);
+
+    const SSRComponents = processComponent(rootComponent, SSRNodesList);
+    const CSRComponents = processComponent(rootComponent, CSRNodesList);
+    const result = {
+      version: "2",
+      SSR: SSRComponents.children,
+      CSR: CSRComponents.children,
+    };
+    return result;
   }
   const customSerialize = () => {
     let craftSearialise = JSON.parse(query.serialize());
     // console.log("previous", craftSearialise);
     const transformedJson = transformJson(craftSearialise);
-    console.log("JSON : ", JSON.stringify(transformedJson, null, 2));
+    // console.log("JSON : ", JSON.stringify(transformedJson, null, 2));
     return transformedJson;
   };
   return (
     <button
       id="saveButtonPageBuilder"
       className="btn btn-primary  mt-3 mx-2"
-      // style={{ display: "none" }}
+      style={{ display: "none" }}
       onClick={() => {
         const customizedJson = customSerialize();
         const compresseddata = lz.encodeBase64(lz.compress(query.serialize()));
@@ -83,9 +123,9 @@ export const PrintJson = () => {
           compressed: compresseddata,
         };
         const data = JSON.stringify(toStoreInDB, null, 2);
-        // document.querySelector("#template").value = data;
-        console.log("toStoreInDB", data);
-        localStorage.setItem("data", data); //api call to save the json and compressed data
+        document.querySelector("#template").value = data;
+        // console.log("toStoreInDB", data);
+        // localStorage.setItem("data", data); //api call to save the json and compressed data
       }}
     >
       Save
